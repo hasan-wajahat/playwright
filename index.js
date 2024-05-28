51151f9ab7fcd7059a3cf96f4dff2f3f4cef8b63
hasan <hasan_wajahat@hotmail.com>
2024-05-28 16:32:07 +0500

    Update playwright

--- index.js
+++ index.js
@@ -5,47 +5,7 @@ import { test } from "./tests/example.js";
 export const handler = async () => {
   console.log("Starting browser");
   // Setup
-  const browser = await chromium.launch({
-    args: [
-      "--autoplay-policy=user-gesture-required",
-      "--disable-background-networking",
-      "--disable-background-timer-throttling",
-      "--disable-backgrounding-occluded-windows",
-      "--disable-breakpad",
-      "--disable-client-side-phishing-detection",
-      "--disable-component-update",
-      "--disable-default-apps",
-      "--disable-dev-shm-usage",
-      "--disable-domain-reliability",
-      "--disable-extensions",
-      "--disable-features=AudioServiceOutOfProcess",
-      "--disable-hang-monitor",
-      "--disable-ipc-flooding-protection",
-      "--disable-notifications",
-      "--disable-offer-store-unmasked-wallet-cards",
-      "--disable-popup-blocking",
-      "--disable-print-preview",
-      "--disable-prompt-on-repost",
-      "--disable-renderer-backgrounding",
-      "--disable-setuid-sandbox",
-      "--disable-speech-api",
-      "--disable-sync",
-      "--disk-cache-size=33554432",
-      "--hide-scrollbars",
-      "--ignore-gpu-blacklist",
-      "--metrics-recording-only",
-      "--mute-audio",
-      "--no-default-browser-check",
-      "--no-first-run",
-      "--no-pings",
-      "--no-sandbox",
-      "--no-zygote",
-      "--password-store=basic",
-      "--use-gl=swiftshader",
-      "--use-mock-keychain",
-      "--single-process",
-    ]
-  });
+  const browser = await chromium.launch({ headless: false});
   const context = await browser.newContext();
   const page = await context.newPage();
 
@@ -55,3 +15,5 @@ export const handler = async () => {
   await context.close();
   await browser.close();
 }
+
+handler();
