a95f022dbc73cee3cd0da9545928a394122896c5
hasan <hasan_wajahat@hotmail.com>
2024-05-28 16:35:47 +0500

    Update playwright

--- tests/example.js
+++ tests/example.js
@@ -7,8 +7,12 @@ import { expect } from '@playwright/test';
  * @param {import("@playwright/test").BrowserContext} context 
  */
 export async function test(page, context) {
-  await page.goto('https://playwright.dev/');
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-  console.log('title found');
+  await page.goto('https://apps.eu.sematext.com/ui/login');
+
+  await page.locator('[data-tid="login-email"]').fill('synthetics+robot@sematext.com')
+  await page.locator('[data-tid="login-password"]').fill('@AGU9xp#NEQA3Z3');
+
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.locator('[data-tid="main-content"]').waitFor();
+  console.log('done');
 }
