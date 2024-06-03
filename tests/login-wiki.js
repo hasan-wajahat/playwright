9f1705578ad75e341bf491a6a570d2e142433504
hasan <hasan_wajahat@hotmail.com>
2024-06-03 18:33:57 +0500

    Improves script

--- tests/login-wiki.js
+++ tests/login-wiki.js
@@ -8,14 +8,11 @@ import { expect } from '@playwright/test';
  */
 export async function testPage(page) {
     await page.goto('https://en.wikipedia.org/');
-
     await page.locator('#pt-login-2').click();
-    await page.waitForURL('**/w/index.php?**');
     const userName = 'Sematext-wiki';
     await page.locator('#wpName1').fill(userName);
     await page.locator('#wpPassword1').fill('semapassword');
     await page.locator('#wpLoginAttempt').click();
-    await page.waitForURL('**/wiki/Main_Page');
     await page.locator('#pt-userpage-2').click();
     const userNameFromPage = await page.locator('#firstHeading').textContent();
     expect(userNameFromPage.split(',')[1].toLowerCase()).toContain(userName.toLowerCase());
